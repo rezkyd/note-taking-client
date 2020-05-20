6d14e30515c00fbb4c34f716fca48e54a230a331
Rezky <rezkydermawan97@gmail.com>
2020-05-20 15:47:36 +0700

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-1",
+      BUCKET: "reskid"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-1",
+      URL: "https://3c86luu2cf.execute-api.ap-southeast-1.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "ap-southeast-1",
+      USER_POOL_ID: "ap-southeast-1_V9gRRPvVV",
+      APP_CLIENT_ID: "5pghk4pt57rfpc4rphv9hqjr2",
+      IDENTITY_POOL_ID: "ap-southeast-1:6f8af693-82dc-456f-b61b-8a7f24885cd4"
+    }
+  };
